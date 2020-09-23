0157d4d1853a170baf3704469499419f91e890bd
Hourann <tdkgn@outlook.com>
2020-09-24 00:39:15 +0800

    improve style

--- src/app/Timer.js
+++ src/app/Timer.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import "../styles/timer.less";
 export default class Timer extends Component {
   state = {
     started: false,
     finished: false,
     remain: 0,
     timer: null,
+    time: 0,
   };
 
   startTimer = (event) => {
@@ -34,29 +36,36 @@ export default class Timer extends Component {
   };
 
   render = () => (
-    <div className="page">
+    <div className="page timer">
       <h1>在线倒计时器</h1>
-      <div className="panel-set">
-        <label htmlFor="time">
-          设置时间
-          <input
-            disabled={this.state.started}
-            id="time"
-            type="number"
-            min="1"
-            onChange={(event) => this.setState({ time: event.target.value })}
-          />
-        </label>
-        <button disabled={this.state.started} onClick={this.startTimer}>
-          Start
-        </button>
-      </div>
-      <div className="panel-display">
-        {this.state.started
-          ? this.state.finished
-            ? "End"
-            : this.state.remain
-          : "Start"}
+
+      <div>
+        <div className="panel-set">
+          <label htmlFor="time">
+            设置时间
+            <input
+              disabled={this.state.started}
+              id="time"
+              type="number"
+              min="1"
+              value={this.state.time}
+              onChange={(event) => this.setState({ time: event.target.value })}
+            />
+          </label>
+          <button
+            disabled={this.state.started || this.state.time <= 0}
+            onClick={this.startTimer}
+          >
+            Start
+          </button>
+        </div>
+        <div className="panel-display">
+          {this.state.started
+            ? this.state.finished
+              ? "End"
+              : `${this.state.remain} Seconds`
+            : "Start"}
+        </div>
       </div>
       <Link to="/">回到主页</Link>
     </div>
--- src/app/WidgetList.js
+++ src/app/WidgetList.js
@@ -6,7 +6,7 @@ import WidgetEntry from "./WidgetEntry";
 
 import "../styles/widget-list.less";
 const WidgetList = () => (
-  <div class="widget-list">
+  <div className="widget-list">
     <WidgetEntry name="计算器" icon={calculatorIcon} path="/calculator" />
     <WidgetEntry name="计时器" icon={timerIcon} path="/timer" />
   </div>
